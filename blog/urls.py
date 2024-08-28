ff8feddfcf69fe89912259fe0503dec94378d535
jjgitit <jaeyeonee0320@gmail.com>
2024-08-28 01:50:47 -0700

    added boostrap for navbar,sidebar,css, passing in made-up data to display on homepage

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+#name should be specific 'blog-home' not home b/c it can collide with others
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author' : 'coreyMS',
+        'title' : 'Blog post 1',
+        'content': 'first post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' : 'Blog post 2',
+        'content': 'second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    } # now we can pass this as a third optional arg
+        #connecting data
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
