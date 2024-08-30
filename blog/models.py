1c8301196e1731e3ab98a5c73bf49b4d8b951d09
jjgitit <jaeyeonee0320@gmail.com>
2024-08-30 01:23:38 -0700

    added login page using bootstrap and displaying posts reading from database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User #this is one to many relation as one person 
+#can post many posts
 
-# Create your models here.
+#we create our data here as classes which is an intuitive way of constructing
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if user get deleted we also delete related posts
+
+    def __str__(self):
+        return self.title
