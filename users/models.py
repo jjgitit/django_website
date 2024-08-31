c63d5a9bdebbc917a55f5b89074d23d5ee1b0a9d
jjgitit <jaeyeonee0320@gmail.com>
2024-08-30 23:49:12 -0700

    added default profile picture and setting profile pic for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#making user models to have 1:1 relationship with profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # remember cascade is when user get deleted you also delete the profile
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user} Profile'
