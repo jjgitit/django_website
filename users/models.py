2b914da8f8418eac8667e74ba4753d50e23adb0d
jjgitit <jaeyeonee0320@gmail.com>
2024-09-10 00:21:34 -0700

    added infrastructure for pubslishing. Setup server with Hezner

--- users/models.py
+++ users/models.py
@@ -1,18 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from PIL import Image
 
-#making user models to have 1:1 relationship with profile
+
+# making user models to have 1:1 relationship with profile
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # remember cascade is when user get deleted you also delete the profile
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    """for basic profile informaitons"""
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE
+    )  # remember cascade is when user get deleted you also delete the profile
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
     def __str__(self):
-        return f'{self.user} Profile'
-    #this save already exists but we are modifying, it gets run everytime model gets saved
+        return f"{self.user} Profile"
+
+    # this save already exists but we are modifying, it gets run everytime model gets saved
     def save(self):
-        super().save() #call the parent save method first
-        img = Image.open(self.image.path) #save the current saving img to resize
+        super().save()  # call the parent save method first
+        img = Image.open(self.image.path)  # save the current saving img to resize
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
--- users/views.py
+++ users/views.py
@@ -1,37 +1,42 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f'Your Account has been Created! You can login now.')
-            return redirect('login')
+            messages.success(
+                request, "Your Account has been Created! You can login now."
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form' : form})
+    return render(request, "users/register.html", {"form": form})
+
 
 @login_required
 def profile(request):
     if request.method == "POST":
-        u_form = UserUpdateForm(request.POST, instance=request.user)#fill in existing info 
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+        u_form = UserUpdateForm(
+            request.POST, instance=request.user
+        )  # fill in existing info
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your Account has been updated!')
-            return redirect('profile')
+            messages.success(request, "Your Account has been updated!")
+            return redirect("profile")
     else:
-        u_form = UserUpdateForm(instance=request.user) #to fill in user info when they want to update
+        u_form = UserUpdateForm(
+            instance=request.user
+        )  # to fill in user info when they want to update
         p_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {
-        'u_form' : u_form,
-        'p_form' : p_form
-    }
-    return render(request, 'users/profile.html', context)
+    context = {"u_form": u_form, "p_form": p_form}
+    return render(request, "users/profile.html", context)
